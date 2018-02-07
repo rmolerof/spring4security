d248637486375fec811f43559e6d7f9001e27678
mecam <mecam@DESKTOP-VB69DUE.fios-router.home>
2018-02-06 23:19:36 -0500

    BuildingAnApp done

--- src/main/resources/static/components/react-components.js
+++ src/main/resources/static/components/react-components.js
@@ -1,24 +1,42 @@
-class StoryBox extends React.Component {
+class Comment extends React.Component {
 	render (){
 		
-		const now = new Date();
-		const topicsList = ['html', 'javascript', 'React'];
 		
 		return (
-				<div className="is-tasty-pie">
-					<h3>Stories App</h3>
-					<p className="lead">
-						Current time: {now.toTimeString()}
-					</p>
-					<h3>Topics List</h3>
-					<ul>{topicsList.map((topic)=><li>{topic}</li>)}</ul>
+				<div className="comment">
+					<div className="note note-info">
+						<p className="comment-header">{this.props.author}</p>
+						<p className="comment-body">
+							{this.props.body}
+						</p>
+						<div className="comment-actions">
+							<a href="#" className="comment-footer-delete">
+								Delete comment
+							</a>
+						</div>
+					</div>
 				</div>
 		);
+	}
+}
+
+class CommentBox extends React.Component{
+	render(){
+		return(
+				<div className="comment-box">
+					<h3>Comments</h3>
+					<h4 className="comment-count">2 comments</h4>
+					<div className="comment-list">
+						<Comment author="Morgan Freeman" body="great picture"/>
+						<Comment author="Bending Bender" body="Excellent statue"/>
+					</div>
+				</div>
+				);
 	}
 }
 
 let target = document.getElementById('story-app');
 
 ReactDOM.render(
-		<StoryBox/>, target
+		<CommentBox/>, target
 );
